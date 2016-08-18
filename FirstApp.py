37848c39fbcab792b8f6c9ec8d099cb893f83275
Gal Weinstock <wein88@gmail.com>
2016-08-18 13:05:00 +0300

    removed the set arena functions

--- FirstApp.py
+++ FirstApp.py
@@ -8,10 +8,6 @@ wlbt.SetSettingsFolder()
 wlbt.ConnectAny()
 
 wlbt.SetProfile(wlbt.PROF_SENSOR)
-wlbt.SetArenaR(10, 50, 5)
-wlbt.SetArenaTheta(-15, 15, 10)
-wlbt.SetArenaPhi(-30, 30, 5)
-wlbt.SetThreshold(15)
 wlbt.SetDynamicImageFilter(wlbt.FILTER_TYPE_MTI)
 
 wlbt.Start()
